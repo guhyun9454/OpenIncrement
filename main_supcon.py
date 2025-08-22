4f3687662ba773aeda5a30f47b47ad0cde221fa0
guhyun9454 <guhyun5454@gmail.com>
2025-08-22 16:04:17 +0900

    tensorboard 로깅 삭제

--- main_supcon.py
+++ main_supcon.py
@@ -283,9 +283,6 @@ def main():
     # build optimizer
     optimizer = set_optimizer(opt, model)
 
-    # tensorboard
-    logger = tb_logger.Logger(logdir=opt.tb_folder, flush_secs=2)
-
     # training routine
     for epoch in range(1, opt.epochs + 1):
         #adjust_learning_rate(opt, optimizer, epoch)
@@ -295,10 +292,6 @@ def main():
         loss = train(train_loader, model, criterion, optimizer, epoch, opt)
         time2 = time.time()
         print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
-
-        # tensorboard logger
-        logger.log_value('loss', loss, epoch)
-        logger.log_value('learning_rate', optimizer.param_groups[0]['lr'], epoch)
 
         if epoch % opt.save_freq == 0:
             save_file = os.path.join(
