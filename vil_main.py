44c6cbeb1f06604e2d3f2d55f414e99f826c586a
Kwon GuHyeon <guhyun5454@gmail.com>
2025-09-04 21:19:14 +0900

    fix: chore

--- vil_main.py
+++ vil_main.py
@@ -198,6 +198,7 @@ def main():
     args = parse_args()
     set_seed(args.seed)
     args = set_data_config(args)
+    args.verbose = True
 
     device = torch.device(args.device if torch.cuda.is_available() else 'cpu')
 
